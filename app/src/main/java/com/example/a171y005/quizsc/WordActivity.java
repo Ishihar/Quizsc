fe45ad89a4801f78a4c47d6e7505bb5dd4bce131
Kenta Ishihara <171Y005@epson-isc.com>
2018-07-18 16:24:19 +0900

    WordControlO MainActivityX

--- app/src/main/java/com/example/a171y005/quizsc/WordActivity.java
+++ app/src/main/java/com/example/a171y005/quizsc/WordActivity.java
@@ -1,13 +1,14 @@
 package com.example.a171y005.quizsc;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -20,12 +21,12 @@ import static com.example.a171y005.quizsc.R.id.word;
 
 public class WordActivity extends AppCompatActivity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate();
-        return super.onCreateOptionsMenu(menu);
-    }
+    private ArrayList<HashMap<String,String>> list_data;
+    private SimpleAdapter sim;
+    private String title;
+    private int pos;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,39 +34,36 @@ public class WordActivity extends AppCompatActivity {
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor c;
-        ArrayList<HashMap<String,String>> list_data = new ArrayList<HashMap<String, String>>();
         HashMap<String,String> hashMap = new HashMap<String, String>();
-
-        final AlertDialog.Builder builder = new AlertDialog.Builder(WordActivity.this);
-
         final ListView list;
-        //ArrayAdapter listadapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
 
         c = db.rawQuery("Select Title,Ans from quiz_table order by Title;",null);
         c.moveToFirst();
+        list_data = new ArrayList<HashMap<String, String>>();
+        sim = new SimpleAdapter(getApplicationContext(),list_data,R.layout.lv_layout,new String[]{"main","right"},new int[]{word, R.id.mean});
         for(int i = 0; i < c.getCount(); i++) {
             hashMap.put("main",c.getString(c.getColumnIndex("Title")));
             hashMap.put("right",c.getString(c.getColumnIndex("Ans")));
             list_data.add(new HashMap<String, String>(hashMap));
             hashMap.clear();
-            //listadapter.add(c.getString(c.getColumnIndex("Title")));
             c.moveToNext();
         }
-        SimpleAdapter sim = new SimpleAdapter(getApplicationContext(),list_data,R.layout.lv_layout,new String[]{"main","right"},new int[]{word, R.id.mean});
+
         list = (ListView)findViewById(R.id.listview);
         list.setAdapter(sim);
+        registerForContextMenu(list);
 
         c.close();
-
         db.close();
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 int cnt = 0;
                 ListView listView = (ListView) parent;
                 Object item = ((ListView) parent).getItemAtPosition(position);
                 String word1 = item.toString();
+
                 for(int i = 0; i < word1.length(); i++){
                     String check = word1.substring(i,i+1);
                     if(check.equals(",")){
@@ -85,14 +83,68 @@ public class WordActivity extends AppCompatActivity {
                 c.moveToFirst();
 
                Log.d("sql","word=" + word1);
-                builder.setTitle("単語管理");
-                builder.setPositiveButton("OK",null);
-                builder.setMessage("内容: " + c.getString(c.getColumnIndex("Title")) + "\n意味: " + c.getString(c.getColumnIndex("Ans")));
-                builder.show();
 
                 c.close();
                 db.close();
             }
-        });
+        });*/
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if(v.getId() == R.id.listview){
+            menu.setHeaderTitle("選択");
+            menu.add(0,0,0,"削除");
+            menu.add(0,1,0,"修正");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        AlertDialog.Builder builder = new AlertDialog.Builder(WordActivity.this);
+        int cnt = 0;
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor c;
+        title = sim.getItem(info.position).toString();
+        for(int i = 0; i < title.length(); i++){
+            String check = title.substring(i,i+1);
+            if(check.equals(",")){
+                break;
+            }
+            cnt++;
+        }
+        title = title.substring(6,cnt);
+        pos = info.position;
+
+        Log.d("test","=" + title);
+        if(item.getItemId() == 0){
+            builder.setTitle("単語削除");
+            builder.setMessage(title + "を削除します。");
+
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Cursor c_delete;
+                    DatabaseHelper dbHelper = new DatabaseHelper(WordActivity.this);
+                    SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+                    c_delete = db.rawQuery("Delete from quiz_table where Title = '" + title + "';", null);
+                    c_delete.moveToFirst();
+                    list_data.remove(pos);
+                    sim.notifyDataSetChanged();
+                }
+            }).setNegativeButton("キャンセル",null).show();
+
+            return true;
+        }
+        else if(item.getItemId() == 1){
+            Log.d("test","修正");
+        }
+
+
+        return super.onContextItemSelected(item);
     }
 }
